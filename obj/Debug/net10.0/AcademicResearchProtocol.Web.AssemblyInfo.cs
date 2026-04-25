fb8a71194c802505fafd3af30070b5878cdb835c
Alket Sula <alket.sula21@umt.edu.al>
2026-04-25 14:44:12 +0200

    second fetching

--- obj/Debug/net10.0/AcademicResearchProtocol.Web.AssemblyInfo.cs
+++ obj/Debug/net10.0/AcademicResearchProtocol.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcademicResearchProtocol.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704e130df9d943a20f2aec760f97292b4279aba6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe2a342560cc84e8254ff9cf02ae3a70de126b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcademicResearchProtocol.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcademicResearchProtocol.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
